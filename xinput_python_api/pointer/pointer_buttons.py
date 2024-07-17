b3109e7f4c8770b3f704678d75b4dd3412f38ba1
taaaf11 <taafuuu@gmail.com>
2024-07-17 16:36:19 +0500

    code organize into classes; formatting

--- xinput_python_api/pointer/pointer_buttons.py
+++ xinput_python_api/pointer/pointer_buttons.py
@@ -1,25 +1,30 @@
 import subprocess
 
+
 class XPointerButtons:
     def __init__(self, dev_id: int, buttons_map: dict, debug=False):
         self.dev_id = dev_id
         self.buttons_map = buttons_map
         self.debug = debug
-    
+
     def _commit(self):
         button_states = self.buttons_map.values()
-        button_states_str = ' '.join([str(_) for _ in button_states])
+        button_states_str = " ".join([str(_) for _ in button_states])
 
         if self.debug:
             return button_states_str
         else:
-            subprocess.run(['xinput', 'set-button-map', str(self.dev_id), button_states_str])
+            subprocess.run(
+                ["xinput", "set-button-map", str(self.dev_id), button_states_str]
+            )
 
     def enable_single(self, button_label: str):
-        self.buttons_map[button_label] = list(self.buttons_map.keys()).index(button_label) + 1
-        
+        self.buttons_map[button_label] = (
+            list(self.buttons_map.keys()).index(button_label) + 1
+        )
+
         self._commit()
-    
+
     def enable_multiple(self, button_labels: list[str]):
         for label in button_labels:
             self.buttons_map[label] = list(self.buttons_map.keys()).index(label) + 1
--- xinput_python_api/pointer/x_pointer.py
+++ xinput_python_api/pointer/x_pointer.py
@@ -1,21 +1,25 @@
-from ..parser import get_mouse_devices_data
-from .pointer_buttons import XPointerButtons
 import subprocess
 
+from ..parser import get_devices_data
+from ..device.x_device import XInputDevice
+from .pointer_buttons import XPointerButtons
 
-class XPointer:
+
+class XPointer(XInputDevice):
     def __init__(self, pointer_data: dict, debug=False):
-        self.id = pointer_data['id']
-        self.name = pointer_data['device_name']
-        self.buttons_map = XPointerButtons(pointer_data['id'], pointer_data['button_map'], debug=debug)
-        
+        super().__init__(pointer_data)
+        self.buttons_map = XPointerButtons(
+            pointer_data["id"], pointer_data["button_map"], debug=debug
+        )
+
     def __getattr__(self, attr):
-        if attr.startswith('disable_'):
-            button_label_to_disable = attr.removeprefix('disable_')
+        if attr.startswith("disable_"):
+            button_label_to_disable = attr.removeprefix("disable_")
             return lambda: self.buttons_map.disable_single(button_label_to_disable)
-        elif attr.startswith('enable_'):
-            button_label_to_enable = attr.removeprefix('enable_')
+        elif attr.startswith("enable_"):
+            button_label_to_enable = attr.removeprefix("enable_")
             return lambda: self.buttons_map.enable_single(button_label_to_enable)
+
 
 def get_mouse_pointers(pointers_data: dict, debug=False):
     pointers = []
