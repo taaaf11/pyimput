af8e6590ccaea6c5235321764fb03f8d247b0462
taaaf11 <taafuuu@gmail.com>
2024-07-17 23:16:05 +0500

    Better approach towards enabling/disabling pointer buttons

--- xinput_python_api/pointer/pointer_buttons.py
+++ xinput_python_api/pointer/pointer_buttons.py
@@ -4,43 +4,39 @@ import subprocess
 class XPointerButtons:
     def __init__(self, dev_id: int, buttons_map: dict, debug=False):
         self.dev_id = dev_id
-        self.buttons_map = buttons_map
+        self.__buttons_map = buttons_map
         self.debug = debug
+    
+    @property
+    def buttons_map(self):
+        return self.__buttons_map
 
-    def _commit(self):
+    def commit(self):
         button_states = self.buttons_map.values()
         button_states_str = " ".join([str(_) for _ in button_states])
 
         if self.debug:
-            return button_states_str
+            print(button_states_str)
         else:
             subprocess.run(
                 ["xinput", "set-button-map", str(self.dev_id), button_states_str]
             )
 
     def enable_single(self, button_label: str):
-        self.buttons_map[button_label] = (
+        self.__buttons_map[button_label] = (
             list(self.buttons_map.keys()).index(button_label) + 1
         )
 
-        self._commit()
-
     def enable_multiple(self, button_labels: list[str]):
         for label in button_labels:
-            self.buttons_map[label] = list(self.buttons_map.keys()).index(label) + 1
-
-        self._commit()
+            self.__buttons_map[label] = list(self.buttons_map.keys()).index(label) + 1
 
     def disable_single(self, button_label: str):
-        self.buttons_map[button_label] = 0
-
-        self._commit()
+        self.__buttons_map[button_label] = 0
 
     def disable_multiple(self, button_labels: list[str]):
         for label in labels:
-            self.buttons_map[label] = 0
-
-        self._commit()
+            self.__buttons_map[label] = 0
 
     def __repr__(self):
-        return f"XPointerButtons(dev_id={self.dev_id}, buttons_map={self.buttons_map.values()})"
+        return f"XPointerButtons(dev_id={self.dev_id}, buttons_map={list(self.buttons_map.values())})"
--- xinput_python_api/pointer/x_pointer.py
+++ xinput_python_api/pointer/x_pointer.py
@@ -10,18 +10,24 @@ class XPointer(XInputDevice):
         self.__buttons_map = XPointerButtons(
             pointer_data["id"], pointer_data["button_map"], debug=debug
         )
+        self.change_button_map = lambda: setattr(self, '__buttons_map', XPointerButtons(pointer_data["id"], pointer_data["button_map"], self.__buttons_map.buttons_map))
 
     @property
     def buttons_map(self):
         return self.__buttons_map
 
     def __getattr__(self, attr):
-        if attr.startswith("disable_"):
-            button_label_to_disable = attr.removeprefix("disable_")
-            return lambda: self.buttons_map.disable_single(button_label_to_disable)
-        elif attr.startswith("enable_"):
-            button_label_to_enable = attr.removeprefix("enable_")
-            return lambda: self.buttons_map.enable_single(button_label_to_enable)
+        if attr.startswith("enable_") or attr.startswitch("disable_"):
+            if attr.startswith("disable_"):
+                button_label_to_disable = attr.removeprefix("disable_")
+                self.buttons_map.disable_single(button_label_to_disable)
+            elif attr.startswith("enable_"):
+                button_label_to_enable = attr.removeprefix("enable_")
+                self.buttons_map.enable_single(button_label_to_enable)
+            self.change_button_map()
+
+            return lambda: self.buttons_map.commit()
+        raise AttributeError(f"'{self.__name__}' has no attribute '{attr}'")
 
     def __repr__(self):
         return f"XPointer(id={self.id}, name={self.name}, master_id={self.master_id}, buttons_map={self.buttons_map})"
